9b1d8eac1e90510491acebc913d8611baca10675
klondikebar28 <124081839+klondikebar28@users.noreply.github.com>
2023-02-01 10:42:49 -0700

    Create fortnite.py

---
+++ fortnite.py
@@ -0,0 +1,6 @@
+gaming = input("are you gaming? ")
+if gaming == "hi":
+  print ("ortnite")
+if gaming == "no":
+  print ("i jhope you die large death horrible death very gruesome and disgusting and horrid. your death will be just about as bad as that one time i ate poo. maybe solid rocks will start coming out of your throat and ass, and nothing will stop this rain of terror. you will bleed until the end, and after the end, you will continue to bleed, feeding the people around you with your bountiful blood. you will perish, but you will continue to feel all of the pain you originally felt. this is your fate")
+  
